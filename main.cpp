2116368954512c1a8c9800fb2e0120aa3e5adeae
daren866 <105366647+daren866@users.noreply.github.com>
2026-06-15 21:52:26 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,17 +9,24 @@ HWND hFileEdit;
 HWND hRenderButton;
 HWND hLoadButton;
 
+// 将 ANSI 字符串转换为宽字符串
+std::wstring ToWideString(const char* str) {
+    int len = MultiByteToWideChar(CP_UTF8, 0, str, -1, NULL, 0);
+    std::wstring wstr(len, 0);
+    MultiByteToWideChar(CP_UTF8, 0, str, -1, &wstr[0], len);
+    return wstr;
+}
+
 // 窗口过程函数
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
         case WM_CREATE:
             // 创建渲染区域
-            hRenderArea = CreateWindowEx(
-                WS_EX_CLIENTEDGE,
-                "STATIC",
-                "",
+            hRenderArea = CreateWindowW(
+                L"STATIC",
+                L"",
                 WS_CHILD | WS_VISIBLE | SS_BLACKFRAME,
-                10, 50, 560, 380,  // 位置和大小
+                10, 50, 560, 380,
                 hWnd,
                 (HMENU)1,
                 ((LPCREATESTRUCT)lParam)->hInstance,
@@ -27,10 +34,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             );
 
             // 创建文件编辑框
-            hFileEdit = CreateWindowEx(
-                WS_EX_CLIENTEDGE,
-                "EDIT",
-                "C:\\xxx\\xxx.sb3",
+            hFileEdit = CreateWindowW(
+                L"EDIT",
+                L"C:\\xxx\\xxx.sb3",
                 WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL,
                 600, 50, 250, 25,
                 hWnd,
@@ -40,10 +46,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             );
 
             // 创建渲染按钮
-            hRenderButton = CreateWindowEx(
-                0,
-                "BUTTON",
-                "渲染",
+            hRenderButton = CreateWindowW(
+                L"BUTTON",
+                L"渲染",
                 WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
                 600, 100, 120, 30,
                 hWnd,
@@ -53,10 +58,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             );
 
             // 创建加载按钮
-            hLoadButton = CreateWindowEx(
-                0,
-                "BUTTON",
-                "加载",
+            hLoadButton = CreateWindowW(
+                L"BUTTON",
+                L"加载",
                 WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
                 740, 100, 120, 30,
                 hWnd,
@@ -64,16 +68,15 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
                 ((LPCREATESTRUCT)lParam)->hInstance,
                 NULL
             );
-
             break;
 
         case WM_COMMAND:
             switch (LOWORD(wParam)) {
-                case 3:  // 渲染按钮
-                    MessageBox(hWnd, "渲染功能", "提示", MB_OK);
+                case 3:
+                    MessageBoxW(hWnd, L"渲染功能", L"提示", MB_OK);
                     break;
-                case 4:  // 加载按钮
-                    MessageBox(hWnd, "加载功能", "提示", MB_OK);
+                case 4:
+                    MessageBoxW(hWnd, L"加载功能", L"提示", MB_OK);
                     break;
             }
             break;
@@ -83,27 +86,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             break;
 
         default:
-            return DefWindowProc(hWnd, message, wParam, lParam);
+            return DefWindowProcW(hWnd, message, wParam, lParam);
     }
     return 0;
 }
 
 // 注册窗口类
 ATOM MyRegisterClass(HINSTANCE hInstance) {
-    WNDCLASSEX wcex;
-    wcex.cbSize = sizeof(WNDCLASSEX);
+    WNDCLASSEXW wcex;
+    wcex.cbSize = sizeof(WNDCLASSEXW);
     wcex.style = CS_HREDRAW | CS_VREDRAW;
     wcex.lpfnWndProc = WndProc;
     wcex.cbClsExtra = 0;
     wcex.cbWndExtra = 0;
     wcex.hInstance = hInstance;
-    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
-    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hIcon = LoadIconW(hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursorW(NULL, IDC_ARROW);
     wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
     wcex.lpszMenuName = NULL;
-    wcex.lpszClassName = "GDScratchWindowClass";
-    wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
-    return RegisterClassEx(&wcex);
+    wcex.lpszClassName = L"GDScratchWindowClass";
+    wcex.hIconSm = LoadIconW(wcex.hInstance, IDI_APPLICATION);
+    return RegisterClassExW(&wcex);
 }
 
 // WinMain 函数
@@ -117,12 +120,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     MyRegisterClass(hInstance);
 
     // 创建主窗口
-    hMainWnd = CreateWindow(
-        "GDScratchWindowClass",
-        "舞台渲染画面",
+    hMainWnd = CreateWindowW(
+        L"GDScratchWindowClass",
+        L"舞台渲染画面",
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, 0,
-        900, 500,  // 窗口大小 (4:3 比例)
+        900, 500,
         NULL,
         NULL,
         hInstance,
